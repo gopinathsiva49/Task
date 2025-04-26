7ee635bf3bf884431fd24b2dbb1597567caecce3
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-04-26 17:48:45 +0530

    added single value - positve flow

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,6 +3,6 @@ require './string_calculator'
 RSpec.describe StringCalculator do
   it "returns 0 for an empty string" do
     calculator = StringCalculator.new
-    expect(calculator.add("")).to eq(0)
+    expect(calculator.add("1")).to eq(1)
   end
 end
